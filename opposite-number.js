6ba9d48ec1ac81f9209efa5e253981c91a095157
khaledessam2 <khaledessam3000@gmail.com>
2026-04-07 12:18:23 +0200

    'codewars

---
+++ opposite-number.js
@@ -0,0 +1,8 @@
+// given an integer on floating point number ; find its opposite => 1 => -1 so on ...
+
+function opposite(number){
+    return number * -1 ;
+}
+console.log(opposite(1)) ;
+console.log(opposite(-5)) ;
+console.log(opposite(10.5)) ;
